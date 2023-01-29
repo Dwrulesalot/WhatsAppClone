7cb2a19f0f3e5e7ebc75453916eee50cb3d5e17c
Daniel <danielw96@hotmail.com>
2023-01-28 19:25:37 -0500

    Chat screen UI
    keyboard now visible/functioning on both android and ios

--- screens/ChatScreen.js
+++ screens/ChatScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useCallback, useState} from "react";
 import {
   View,
   Text,
@@ -7,30 +7,77 @@ import {
   ImageBackground,
   TextInput,
   TouchableOpacity,
+  KeyboardAvoidingView,
+  Platform,
 } from "react-native";
 import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from '@expo/vector-icons';
 
 import backgroundImage from "../assets/images/droplet.jpeg";
 import colors from "../constants/colors";//
 
 const ChatScreen = (props) => {
+
+  const [messageText, setMessageText] = useState("");
+  //console.log(messageText);
+
+  //re renders when message texts changes
+  const sendMessage = useCallback(() => {
+    setMessageText("");//temp to clear message
+
+  }, [messageText]);
+
+  //Keyboard avoiding view fix for both Ios and android so that the keyboard works as intended
   return (
+  <KeyboardAvoidingView 
+  style={styles.keyboardScreen}
+  behavior={Platform.OS === "ios" ? "padding" : undefined}
+  keyboardVerticalOffset={100}
+  > 
     <View style={styles.container}>
       <ImageBackground
         source={backgroundImage}
         style={styles.backgroundImage}
       />
+
       <View style={styles.inputContainer}>
-        <TouchableOpacity onPress={() => console.log("Attach button press")}>
+        
+
+        <TouchableOpacity 
+        style={styles.buttonStyle}
+        onPress={() => console.log("Attach button press")}>
           <AntDesign name="plus" size={28} color="black" />
         </TouchableOpacity>
 
-        <TextInput style={styles.textInput} />
-        <TouchableOpacity onPress={() => console.log("Camera button press")}>
+        <TextInput 
+        style={styles.textInput} 
+        value={messageText}
+        onChangeText={text => setMessageText(text)}
+        onSubmitEditing={sendMessage}
+        />
+        
+      
+        {
+        messageText === "" && //if message text is empty shows camera button
+        <TouchableOpacity 
+        style={styles.buttonStyle}
+        onPress={() => console.log("Camera button press")}>
           <AntDesign name="camera" size={28} color="black" />
         </TouchableOpacity>
+        }
+
+       {
+        messageText !== "" && //if message contains text shows send button
+        <TouchableOpacity 
+        style={{...styles.buttonStyle, ...styles.sendButton}}//allows send button to be adjusted with all buttons if needed or just adjusted itself ie color etc
+        onPress={sendMessage}>
+          <Ionicons name="send" size={20} color="white" />
+        </TouchableOpacity>
+        }
+      
       </View>
     </View>
+    </KeyboardAvoidingView>
   );
 };
 
@@ -38,6 +85,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     flexDirection: "column",
+    backgroundColor: colors.lightGreen,//how do i change color at the top where the the title is
+  },
+  keyboardScreen: {
+    flex: 1,
   },
   backgroundImage: {
     flex: 1,
@@ -46,14 +97,26 @@ const styles = StyleSheet.create({
     flexDirection: "row",
     paddingVertical: 8,
     paddingHorizontal: 10,
-    backgroundColor: colors.lightGreen
+    backgroundColor: colors.lightGreen,
   },
-  textInput: {
+  textInput: {//how do i get this to expand vertically rather than the text going off the screen?
     flex: 1,
     borderWidth: 1,
     borderRadius: 50,
     backgroundColor: colors.lightGrey,
-    paddingHorizontal: 5
+    paddingHorizontal: 5, 
+    marginHorizontal: 10
+  },
+  buttonStyle: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 30
+  },
+  sendButton: {
+    backgroundColor: colors.blue,
+    borderRadius: 50,
+    paddingLeft: 2
+
   }
 });
 
