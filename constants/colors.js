aad354fc2842ac5fa4903dc081a7364ef2539df9
Daniel <danielw96@hotmail.com>
2023-03-25 14:38:00 -0400

    added colors

--- constants/colors.js
+++ constants/colors.js
@@ -2,5 +2,7 @@ export default{
     //add colors here ie:
     blue: "#3498db",
     lightGrey: '#bdc3c7',
-    lightGreen: '#a4e4a0'
+    grey: '#7f8c8d',
+    lightGreen: '#a4e4a0',
+    offWhite: '#F4F8F7'
 }
