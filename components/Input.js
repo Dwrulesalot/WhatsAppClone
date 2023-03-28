e14068dee92a85d2f5aeb3478f4d43ff499a8e75
Daniel <danielw96@hotmail.com>
2023-03-27 23:47:16 -0400

    completed modular inputs with basic error handling

--- components/Input.js
+++ components/Input.js
@@ -5,20 +5,31 @@ import { FontAwesome } from "@expo/vector-icons";
 const Input = (props) => {
   return (
     <View style={styles.container}>
-        <Text>{props.label}</Text>
+        <Text style={styles.label}>{props.label}</Text>
 
         <View style={styles.inputContainer}>
             {
             //below allows for component to still be used without any icon
             props.icon && <props.iconPack 
             name={props.icon} 
-            size={24} 
+            size={props.iconSize || 20} 
             style={styles.icon} 
             />
             }
 
-            <TextInput />
+            <TextInput style={styles.textInput} />
         </View>
+
+        {
+            props.errorText &&
+            <View style={styles.errorContainer}>
+            <Text style={styles.errorText}>{props.errorText}</Text>
+
+        </View>
+        }
+
+
+
     </View>
   );
 };
@@ -34,11 +45,36 @@ const styles = StyleSheet.create({
     borderRadius: 2,
     backgroundColor: colors.offWhite,
     flexDirection: "row",
+    alignItems: "center",
   },
   icon: {
     marginRight: 10,
     color: colors.grey 
-  }
+  },
+  label: {
+    marginVertical: 8,
+    fontFamily: 'bold',
+    letterSpacing: 0.3,
+    color: colors.textColor,
+
+  },
+  textInput: {
+    color: colors.textColor,
+    flex: 1,
+    fontFamily: 'regular',
+    letterSpacing: 0.3,
+    paddingTop: 0,
+  },
+  errorContainer: {
+    marginVertical: 5
+  },
+  errorText: {
+    color: 'red',
+    fontSize: 13,
+    fontFamily: "regular",
+    letterSpacing: 0.3,
+
+  },
 });
 
 export default Input;
--- screens/AuthScreen.js
+++ screens/AuthScreen.js
@@ -1,4 +1,6 @@
 import { FontAwesome } from "@expo/vector-icons";
+import { Entypo } from '@expo/vector-icons';
+import { Feather } from '@expo/vector-icons';
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -9,7 +11,30 @@ const AuthScreen = props => {
   return <SafeAreaView style={{ flex:1 }}>
       <PageContainer>
 
-        <Input label="First Name" icon= "user-circle-o" iconPack={FontAwesome}/>
+        <Input 
+        label="First Name" 
+        icon= "user-circle-o" 
+        iconPack={FontAwesome} 
+        />
+
+        <Input 
+        label="Last Name" 
+        icon= "user-circle-o" 
+        iconPack={FontAwesome} 
+        />
+
+        <Input 
+        label="Email" 
+        icon= "mail" 
+        iconPack={Feather} 
+        />
+
+        <Input 
+        label="Password" 
+        icon= "lock" 
+        iconPack={Entypo} 
+        />
+        
 
       </PageContainer>
     </SafeAreaView>
