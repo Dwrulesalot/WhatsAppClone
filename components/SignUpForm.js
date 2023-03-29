6bdd59a87aceb3bc8feb5dbc246cedf07cbb4307
Daniel <danielw96@hotmail.com>
2023-03-28 22:50:02 -0400

    migrated form to it's own component
    error right now?

---
+++ components/SignUpForm.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Input from "./Input";
+import SubmitButton from "./SubmitButton";
+import { View} from "react-native";
+
+const SignUpForm = props => {
+return {
+    <View>
+        <Input 
+            label="First Name" 
+            icon= "user-circle-o" 
+            iconPack={FontAwesome} 
+        />
+
+        <Input 
+            label="Last Name" 
+            icon= "user-circle-o" 
+            iconPack={FontAwesome} 
+        />
+
+    <Input 
+    label="Email" 
+    icon= "mail" 
+    iconPack={Feather} 
+    />
+
+    <Input 
+    label="Password" 
+    icon= "lock" 
+    iconPack={Entypo} 
+    />
+    
+    <SubmitButton 
+    title="Sign Up"
+    onPress={() => console.log("Sign UP pressed")}
+    disabled={false}
+    style={styles.submitStyle}
+    />
+    </View>
+}
+};
+
+export default SignUpForm;
